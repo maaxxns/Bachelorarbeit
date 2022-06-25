e0e0e77ae5efbda15757fc2239228b0160f5412d
maaxxns <max.koch@udo.edu>
2022-06-25 14:15:12 +0200

    add power calculations now plot.py makes a plot that shows the peak distances aswell as the electric field power in arb. units this makes it easy to compare the fields in relation to the pump power

--- plot_programm/plot.py
+++ plot_programm/plot.py
@@ -20,38 +20,62 @@ from scipy.signal import find_peaks
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilenames
 import os
-
 root = Tk()
 root.withdraw() # we don't want a full GUI, so keep the root window from appearing
 relative_path = 'plot/home/max/Documents/Bachelorarbeit/Bachelorarbeit/plot_data/'
 filename = askopenfilenames(multiple=True)
 filename = list(filename)
-#print(filename) # show an "Open" dialog box and return the path to the selected file
+
+#just take the filename out of the whole path
 for i in range(len(filename)):
     name = filename[i]
     name = name[name.find('daten'):]
-    filename[i] = name 
-print(filename)
+    filename[i] = name.split('/')[1]
 
-#files = ['13_19_29.txt', '13_21_32.txt', '13_23_05.txt', '13_24_16.txt', '14_07_55.txt', '14_13_51.txt', '15_17_04.txt', '16_01_01.txt']
 savefiles = list(np.zeros(len(filename)))
 
+#make a directory for the plot.pdf and get the propper filenames
 for i in range(len(filename)):
     savefiles[i] = filename[i]+'.pdf'
+    path = 'daten/' +  filename[i].split('.')[0]
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+
+peak_distances = []
+FFT = []
+
+#######################################
+#   functions
+#######################################
+
+#   qudrate and 'integrate'
+
+def power_fft(FFT, integrate=None):
+    if integrate==True:
+        print('not implemented yet')
+        #implement a integratrion function like scipy
+    else:
+        for i in range(len(FFT)):
+            FFT[i] = FFT[i]**2
+            FFT[i] = np.sum(FFT[i])
+        return FFT
 
 
-#savefiles = ['plot14_45_16.pdf', 'plot16_27_14.pdf', 'plot16_21_02.pdf', 'plot16_18_12.pdf']
+
+
 
 for n in (range(len(filename))):
-    t, p, delay, X, Y, R, theta = np.genfromtxt(filename[n], delimiter='\t', unpack=True, skip_header=1) #unpack the txt file with pixel values
+    t, p, delay, X, Y, R, theta = np.genfromtxt( 'daten/' + filename[n], delimiter='\t', unpack=True, skip_header=1) #unpack the txt file with pixel values
                                                                                      #I dont know why it wants to unpack 9 values,but thats the reason for the variable m
     
 
 
 
 
+    print('processing file: ' + filename[n] + '...')
     #############################
-    #Isulate the THz Pulse
+    #Isolate the THz Pulse
     peak = find_peaks(X, distance=100000) #finds the highest peak and returns the index
     delay_peak = delay[peak[0]]
     start_pulse = np.where(delay >= delay_peak-5*10**(-12))[0][0]
@@ -115,6 +139,9 @@ for n in (range(len(filename))):
     data_all = [[data_normal, Fdata_normal, Fdata_normal],[data_normal, FData_zeropadding, FData_zeropadding],[data_normal, FData_pulse, FData_pulse]]
     data_name_all = [data_name_normal, data_name_zeropadding, data_name_pulse]
     savename = ['normal', 'zeropadding', 'pulse']
+    
+
+    #FFT.append(FData_zeropadding)
 
     ######################
     #   Plotting
@@ -136,10 +163,11 @@ for n in (range(len(filename))):
                 ax[i].plot(data_all[k][i][0], data_all[k][i][1], label=data_name_all[k][i])
 
 
-            if data_name_all[k][i] == data_name_pulse:
-                ax[i].vlines(data_all[k][i][0][start_pulse], ymin=-0.00025, ymax= 0.00025,colors='r', label='cut off FFT')
-                ax[i].vlines(data_all[k][i][0][end_pulse], ymin=-0.00025, ymax= 0.00025, colors='r')
-                ax[i].legend()
+            if data_name_all[k] == data_name_pulse:
+                if data_name_all[k][i] == data_name_pulse[0]:
+                    ax[i].vlines(data_all[k][i][0][start_pulse], ymin=-0.00025, ymax= 0.00025,colors='r', label='cut off FFT')
+                    ax[i].vlines(data_all[k][i][0][end_pulse], ymin=-0.00025, ymax= 0.00025, colors='r')
+                    ax[i].legend()
 
             ax[i].grid()
             ax[i].set_xlabel(data_name_x[i])
@@ -148,7 +176,7 @@ for n in (range(len(filename))):
 
 
         plt.tight_layout()
-        plt.savefig(filename[n].split('.')[0]+savename[k]+'.pdf')
+        plt.savefig('daten/' + filename[n].split('.')[0] + '/' +filename[n].split('.')[0]+savename[k]+'.pdf')
         plt.close()
 
 #######################################
@@ -170,6 +198,61 @@ for n in (range(len(filename))):
             plt.ylabel(data_name_y[i])
             plt.title(data_name_all[k][i])
             plt.tight_layout()
-            plt.savefig(filename[n].split('.')[0] + savename[k]+ data_name_all[k][i]+'.pdf')
+            plt.savefig('daten/' + filename[n].split('.')[0] + '/' + filename[n].split('.')[0] + savename[k]+ data_name_all[k][i]+'.pdf')
             plt.clf()
             plt.close(fig=i)
+
+
+
+
+    ###############################
+    #   further calculations
+    ###############################
+        # - i need:
+        # +quadrated fft value of every dataset
+        # +distance between lowest and highest peak
+        # +intergral of quadrated fft values
+
+        ###############
+        #first the distance between peak low and high
+
+    FFT.append(FData_zeropadding[1])
+    peak_height =(np.abs(np.max(X) + np.min(X)))
+    peak_distances.append(peak_height)
+    print('low to high peak distance: ', peak_height)
+
+fig, axis = plt.subplots(2, 1, figsize=(16,8), num=1)
+pump_power = [75, 58.2, 36.80, 26.5, 10.24, 7.28, 45.1]
+
+###########################
+#   peak distances plotted
+###########################
+if filename[0] == '11_46_58.txt' and filename[-1] == '15_53_33.txt':
+    if len(pump_power) == len(peak_distances):
+        axis[0].plot(pump_power, np.array(peak_distances)/np.max(peak_distances),'rx' ,label='peak_distances')
+    else:
+        print('WARNING pump power array diffrent length then peak distances. subplimantary range(len(peakdistances)) was used')
+        axis[0].plot(range(len(peak_distances)), np.array(peak_distances)/np.max(peak_distances),'rx' ,label='peak_distances', )
+    axis[0].grid()
+    axis[0].set_xlabel('pump power/mW')
+    axis[0].set_ylabel('percentage of maximum peak distance')
+    axis[0].set_title('Peak Distances with diffrent Pump Power')
+
+    ##########################
+    #   Power of electric fields plotted
+    ##########################
+
+    if len(pump_power) == len(peak_distances):
+        axis[1].plot(pump_power, peak_distances,'kx', label='peak_distance')
+    else:
+        print('WARNING pump power array diffrent length then peak distances. subplimantary range(len(peakdistances)) was used')
+        axis[1].plot(range(len(peak_distances)), power_fft(FFT),'kx' ,label='peak_distances')
+    axis[1].grid()
+    axis[1].set_xlabel('pump power/mW')
+    axis[1].set_ylabel('Power(THz)/arb. units')
+    axis[1].set_title('power of eletric field')
+    #for i in range(len(filename)):
+        #axis[2].text(100*i, 100*i, filename[i])
+    plt.tight_layout()
+    plt.savefig('daten/Fluence_comparission.pdf')
+    plt.close()
